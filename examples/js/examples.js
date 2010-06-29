bd0b138481f625a3641a69388207a85d209e369b
Gary van Woerkens <gary@chewam.com>
2010-06-29 16:43:03 +0200

    add event on send message

--- examples/js/examples.js
+++ examples/js/examples.js
@@ -3,6 +3,7 @@ Ext.onReady(function() {
     var chat = new Ext.ux.chat.Chat({
         width:400
         ,height:400
+        ,editorHeight:100
     });
 
     chat.render(Ext.getBody());
--- js/Ext.ux.chat.Chat.js
+++ js/Ext.ux.chat.Chat.js
@@ -1,119 +1,149 @@
 Ext.ns("Ext.ux.chat");
 
-Ext.ux.chat.Chat = function(config) {
-    Ext.apply(this, config || {});
-    Ext.ux.chat.Chat.superclass.constructor.call(this);
-    this.addEvents("render");
+if (!Ext.Component) {
 
-    this.on({
-        render:function() {
-            var button = this.getButtonEl();
-            var ctn = Ext.DomQuery.selectNode("div.x-chat-form-editor");
-            var height = Ext.fly(ctn).getHeight();
-            if (Ext.isChrome) height += Ext.fly(ctn).getBorderWidth("tb") + 3;
-            Ext.fly(button).setHeight(height);
-            Ext.fly(button).on({
-                scope:this
-                ,click:this.onButtonClick
-            });
-        }
-    });
+    Ext.ux.chat.Chat = function(config) {
+        Ext.apply(this, config || {});
+        Ext.ux.chat.Chat.superclass.constructor.call(this);
+        this.addEvents("render", "message");
 
-};
-
-Ext.extend(Ext.ux.chat.Chat, Ext.util.Observable, {
-
-    width:0
-
-    ,height:0
-
-    ,editorHeight:100
-
-    ,render:function(el) {
-
-        var dh = Ext.DomHelper;
-
-        var spec = {
-            tag:"div"
-            ,cls:"x-chat"
-            ,style:{
-                width:this.width.toString() + "px"
-                ,height:this.height.toString() + "px"
+        this.on({
+            render:function() {
+                var button = this.getButtonEl();
+                var ctn = Ext.DomQuery.selectNode("div.x-chat-form-editor");
+                var height = Ext.fly(ctn).getHeight();
+                if (Ext.isChrome) height += Ext.fly(ctn).getBorderWidth("tb") + 3;
+                Ext.fly(button).setHeight(height);
+                Ext.fly(button).on({
+                    scope:this
+                    ,click:this.onButtonClick
+                });
             }
-            ,children:[{
+        });
+
+    };
+
+    Ext.extend(Ext.ux.chat.Chat, Ext.util.Observable, {
+
+        width:0
+
+        ,height:0
+
+        ,editorHeight:0
+
+        ,render:function(el) {
+
+            var dh = Ext.DomHelper;
+
+            var spec = {
                 tag:"div"
-                ,cls:"x-chat-list"
+                ,cls:"x-chat"
                 ,style:{
-                    overflow:"auto"
-                    ,height:(this.height - this.editorHeight - 15 - 4).toString() + "px"
-                }
-            }, {
-                tag:"div"
-                ,cls:"x-chat-form"
-                ,style:{
-                    height:this.editorHeight.toString() + "px"
+                    width:this.width.toString() + "px"
+                    ,height:this.height.toString() + "px"
                 }
                 ,children:[{
                     tag:"div"
-                    ,cls:"x-chat-form-editor"
+                    ,cls:"x-chat-list"
                     ,style:{
-                        float:"left"
-                        ,width:(this.width - 100 - 15 - 4).toString() + "px"
-                        ,height:"100%"
+                        overflow:"auto"
+                        ,height:(this.height - this.editorHeight - 15 - 4).toString() + "px"
+                    }
+                }, {
+                    tag:"div"
+                    ,cls:"x-chat-form"
+                    ,style:{
+                        height:this.editorHeight.toString() + "px"
                     }
                     ,children:[{
-                        tag:"textarea"
+                        tag:"div"
+                        ,cls:"x-chat-form-editor"
                         ,style:{
-                            width:"100%"
-                            ,height:"99%"                            
+                            float:"left"
+                            ,width:(this.width - 100 - 15 - 4).toString() + "px"
+                            ,height:"100%"
+                        }
+                        ,children:[{
+                            tag:"textarea"
+                            ,style:{
+                                width:"100%"
+                                ,height:"99%"
+                            }
+                        }]
+                    }, {
+                        tag:"button"
+                        ,html:"Envoyer"
+                        ,cls:"x-chat-form-button"
+                        ,style:{
+                            float:"left"
+                            ,width:"100px"
+                            ,height:"100%"
+                            ,"font-size":"16px"
                         }
                     }]
-                }, {
-                    tag:"button"
-                    ,html:"Envoyer"
-                    ,cls:"x-chat-form-button"
-                    ,style:{
-                        float:"left"
-                        ,width:"100px"
-                        ,height:"100%"
-                        ,"font-size":"16px"
-                    }
                 }]
-            }]
-        };
+            };
 
-        this.el = dh.append(el, spec);
+            this.el = dh.append(el, spec);
 
-        this.fireEvent("render", this);
-    }
+            this.fireEvent("render", this);
+        }
 
-    ,onButtonClick:function() {
-        var editor = this.getEditorEl();
-        var msg = Ext.fly(editor).getValue();
-        this.addMessage({from:"me", msg:msg});
-        Ext.fly(editor).dom.value = "";
-        Ext.fly(editor).focus();
-    }
+        ,onButtonClick:function() {
+            var editor = this.getEditorEl();
+            var msg = Ext.fly(editor).getValue();
+            this.addMessage({from:"me", msg:msg});
+            this.fireEvent("message", this, {from:"me", msg:msg});
+            Ext.fly(editor).dom.value = "";
+            Ext.fly(editor).focus();
+        }
 
-    ,addMessage:function(o) {
-        var list = this.getListEl();
-        Ext.DomHelper.append(Ext.fly(list), {
-            tag:"div"
-            ,cls:"x-chat-list-msg"
-            ,html:o.from + ": " + o.msg
-        });
-    }
+        ,addMessage:function(o) {
+            var list = this.getListEl();
+            Ext.DomHelper.append(Ext.fly(list), {
+                tag:"div"
+                ,cls:"x-chat-list-msg"
+                ,html:o.from + ": " + o.msg
+            });
+        }
 
-    ,getListEl:function() {
-        return Ext.DomQuery.selectNode("div.x-chat-list");
-    }
+        ,getListEl:function() {
+            return Ext.DomQuery.selectNode("div.x-chat-list");
+        }
 
-    ,getEditorEl:function() {
-        return Ext.DomQuery.selectNode("div.x-chat-form-editor textarea");
-    }
+        ,getEditorEl:function() {
+            return Ext.DomQuery.selectNode("div.x-chat-form-editor textarea");
+        }
 
-    ,getButtonEl:function() {
-        return Ext.DomQuery.selectNode("button.x-chat-form-button");
-    }
+        ,getButtonEl:function() {
+            return Ext.DomQuery.selectNode("button.x-chat-form-button");
+        }
 
-});
+    });
+
+} else {
+
+    Ext.ux.chat.Chat = Ext.extend(Ext.Panel, {
+        width:0
+        ,height:0
+        ,editorHeight:0
+
+        ,initComponent:function() {
+
+            Ext.apply(this, {
+                layout:"border"
+                ,items:[{
+                    region:"center"
+                }, {
+                    region:"south"
+                    ,height:this.editorHeight
+                }]
+            });
+
+            Ext.ux.chat.Chat.superclass.initComponent.call(this);
+
+        }
+
+    });
+
+}
