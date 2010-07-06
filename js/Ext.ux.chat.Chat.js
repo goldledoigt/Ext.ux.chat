2ff4795233d895b0a770f346efcf9b81ae06cf87
Gary van Woerkens <gary@revolunet.com>
2010-07-06 16:54:02 +0200

    add webcam

--- js/Ext.ux.chat.Chat.js
+++ js/Ext.ux.chat.Chat.js
@@ -13,6 +13,22 @@ Ext.ux.chat.Chat = Ext.extend(Ext.Panel, {
         Ext.apply(this, {
             layout:"border"
             ,items:[{
+	      region:"north"
+	      ,ref:"camPanel"
+	      ,height:200
+	      ,collapsible:true
+	      ,collapsed:true
+	      ,cls:"x-chat-campanel"
+	      ,cmargins:"5 5 0 5"
+	      ,bodyStyle:"border-width:0 0 1 0"
+	      ,listeners:{
+		scope:this
+		,afterrender:function() {
+		  console.log("afterrender", this, arguments, this.body.id);
+		  this.body.on({scope:this, click:this.onCamPanelClick});
+		}
+	      }
+	    }, {
                 region:"center"
                 ,ref:"list"
                 ,margins:"5"
@@ -40,6 +56,8 @@ Ext.ux.chat.Chat = Ext.extend(Ext.Panel, {
             }]
         });
 
+	this.addEvents("send", "recieve");
+
         Ext.ux.chat.Chat.superclass.initComponent.call(this);
 
     }
@@ -55,14 +73,37 @@ Ext.ux.chat.Chat = Ext.extend(Ext.Panel, {
             ,html:o.from + ": " + o.msg
         });
         this.list.doLayout();
+      this.fireEvent("recieve", this, o);
     }
 
     ,onButtonClick:function() {
         var msg = this.editor.getValue();
         this.addMessage({from:"me", msg:msg});
-        this.fireEvent("message", this, {from:"me", msg:msg});
+        this.fireEvent("send", this, {from:"me", msg:msg});
         this.clearEditor();
         this.editor.focus();
+    }
+
+    ,onCamPanelClick:function() {
+      if (!this.flash) {
+	this.flash = new Ext.FlashComponent({
+	  url:"/apps/whiteboard/static/js/StratusWidget.swf"
+	  ,backgroundColor:"#FFFFFF"
+	  //,flashVersion:"10.0.0"
+	  ,flashVars:{
+	    mode:"master"
+	    ,useVideo:1
+	    ,doStream:1
+	    ,debug:1
+//	    ,height:200
+//	    ,width:300
+	    ,r:Math.random()
+	  }
+	});
+	this.camPanel.add(this.flash);
+	this.camPanel.doLayout();
+	console.log("click", this.flash);
+      }
     }
 
 });
